1e5d69799ae8c656a78cb4ea26b4086437c641c3
Daniel Ward <50274655+Quarantiine@users.noreply.github.com>
2022-09-08 02:54:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -266,3 +266,34 @@ toggleBtns[2].addEventListener('click', ()=> {
     toggleBtns[1].classList.remove('toggle-btn-toggle');
     toggleBtns[0].classList.remove('toggle-btn-toggle');
 });
+
+// cs7-slide-show-container: =====
+const cs7SlideShowContainer = document.querySelector('.cs7-cs2-ic-slide-container');
+const cs7SsBox = document.querySelector('.slide-container-box');
+const cs7ArrowBtns1 = document.querySelector('.l-arr');
+const cs7ArrowBtns2 = document.querySelector('.r-arr');
+let n7 = 300;
+
+const cs7SlideShowSystem = ()=> {
+    cs7SlideShowContainer.scrollTo(n7, 0);
+    n7 == 1500 ? n7 = 0 : n7+=300;
+    // console.log(n7)
+};
+setInterval(()=> {
+    cs7SlideShowSystem()
+}, 10000);
+
+const cs7ArrowBtnsSystem = ()=> {
+    cs7SlideShowContainer.scrollBy(-300, 0);
+};
+const cs7ArrowBtnsSystem2 = ()=> {
+    cs7SlideShowContainer.scrollBy(300, 0);
+};
+
+cs7ArrowBtns1.addEventListener('click', ()=> {
+    cs7ArrowBtnsSystem();
+});
+cs7ArrowBtns2.addEventListener('click', ()=> {
+    cs7ArrowBtnsSystem2();
+});
+
